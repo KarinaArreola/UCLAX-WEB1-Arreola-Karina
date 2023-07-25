9a0ef2b5523ac16018dbc7b081f96301c706c5ed
Ana Arreola <karinaarreola@socal.rr.com>
2023-07-24 21:03:50 -0700

    lesson 5

---
+++ src/Common/PagesLayout/Header/Hamburger.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+const Hamburger = ({ TogleMenu }) => {
+    return (
+        <HamburgerStyled className="Hamburger" onClick={TogleMenu}>
+            <FontAwesomeIcon icon={faBars} />
+        </HamburgerStyled>
+    );
+};
+
+export default Hamburger;
+
+const HamburgerStyled = styled.div`
+    position: absolute;
+    top: 20px;
+    right: 20px;
+
+    font-size: 30px;
+    color: white;
+
+    cursor: pointer;
+`;
