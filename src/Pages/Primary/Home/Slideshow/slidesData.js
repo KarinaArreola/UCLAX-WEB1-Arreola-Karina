eee1990082525ebe71a559a1d193b79b9c283fb9
Ana Arreola <karinaarreola@socal.rr.com>
2023-08-20 16:42:29 -0700

    Update carousel images

--- src/Pages/Primary/Home/Slideshow/slidesData.js
+++ src/Pages/Primary/Home/Slideshow/slidesData.js
@@ -1,33 +1,39 @@
-import slide1 from './img/Core-Strength.jpg';
-import slide2 from './img/Staff-In-Action.jpg';
-import slide3 from './img/Senior.jpg';
-import slide4 from './img/Group-Picture.jpg';
-import slide5 from './img/Aerial-Open-Gym.jpg';
+import slide1 from "./img/adrian-swimming-butterfly.jpg";
+import slide2 from "./img/adrian-swimming-backstroke.jpg";
+import slide3 from "./img/adrian-swimming-breastroke.jpg";
+import slide4 from "./img/adrian-swimming-freestyle.jpg";
+import slide5 from "./img/race-start.jpg";
+import slide6 from "./img/adrian-holding-im-sign.jpg";
 
 export const slidesData = [
     {
         id: 1,
         image: slide1,
-        title: 'Core Strength',
+        title: "Butterfly",
     },
     {
         id: 2,
         image: slide2,
-        title: 'Staff In Action',
+        title: "Backstroke",
     },
     {
         id: 3,
         image: slide3,
-        title: 'Senior',
+        title: "Breastroke",
     },
     {
         id: 4,
         image: slide4,
-        title: 'Group Picture',
+        title: "Freestyle",
     },
     {
         id: 5,
         image: slide5,
-        title: 'Aerial Open Gym',
+        title: "Racing",
     },
-]
+    {
+        id: 6,
+        image: slide6,
+        title: "IM",
+    },
+];
