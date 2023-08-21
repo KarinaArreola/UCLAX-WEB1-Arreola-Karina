083735297d61ae51e856f6ea22a214286b8183c6
Ana Arreola <karinaarreola@socal.rr.com>
2023-08-21 02:40:11 -0700

    Remove button from `Swim Shop` page

--- src/Pages/Primary/Staff/Item/ItemDescription.jsx
+++ src/Pages/Primary/Staff/Item/ItemDescription.jsx
@@ -15,9 +15,6 @@ const Item = ({ member }) => {
                 <b>Price: </b>
                 {member.price}
             </div>
-            <div>
-                <BuyButton type="submit">Add to Cart</BuyButton>
-            </div>
         </ItemStyled>
     );
 };
@@ -28,16 +25,4 @@ const ItemStyled = styled.div`
     padding: 20px;
     font-size: 14px;
     line-height: 1.5rem;
-`;
-
-const BuyButton = styled.button`
-    /* Add your button styling here */
-    background-color: #041e42;
-    color: #ffffff;
-    border: none;
-    padding: 10px 20px;
-    cursor: pointer;
-    border-radius: 4px;
-    font-size: 16px;
-    margin-top: 16px;
 `;
