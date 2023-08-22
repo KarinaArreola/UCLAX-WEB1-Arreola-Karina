96a39ba62a4a6749cafa5b7460c573c3b96ff173
Ana Arreola <karinaarreola@socal.rr.com>
2023-08-21 21:38:06 -0700

    Fix `Carousel` issue

--- src/Pages/Primary/Home/Slideshow/SlideList.jsx
+++ src/Pages/Primary/Home/Slideshow/SlideList.jsx
@@ -1,32 +1,23 @@
 import styled from "styled-components";
-
 /* React Carousel :: 3 rd Pary Library ------------*/
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-
 import { Carousel } from "react-responsive-carousel";
-
 /* Data ------------*/
 import { slidesData } from "./slidesData";
-
 /* Components ------------*/
 import Slide from "./Slide";
-
 const SlideList = () => {
     return (
         <SlideListStyled className="SlideList">
-            {" "}
             <Carousel autoPlay={true}>
-                {" "}
                 {slidesData.map((slide) => {
                     return <Slide key={slide.id} slide={slide} />;
                 })}
-            </Carousel>{" "}
+            </Carousel>
         </SlideListStyled>
     );
 };
-
 export default SlideList;
-
 const SlideListStyled = styled.div`
     .carousel .slide .legend {
         background-color: #041e42;
